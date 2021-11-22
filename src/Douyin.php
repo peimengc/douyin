371f5a803d3ec7deb97568aac3436743f612e5f1
peimeng <931692760@qq.com>
2021-11-22 15:23:53 +0800

    app sms login

--- src/Douyin.php
+++ src/Douyin.php
@@ -3,18 +3,27 @@
 namespace Peimengc\Douyin;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\HandlerStack;
 
 class Douyin
 {
     const CHECK_QRCODE_URI = "https://www.douyin.com/passport/web/check_qrconnect/?aid=1128&next=https%3A%2F%2Fwww.douyin.com%2Flogin%2Fcallback%2F%3Fnext%3Dhttps%253A%252F%252Fwww.douyin.com%252Fpages%252Fdouyin_recharge%252Findex.html&token=";
     const USER_INFO_URI = 'https://creator.douyin.com/web/api/media/user/info/?_signature=_02B4Z6wo00d01QJRSVQAAIDCyu2AS1DahZUCVU3AACBnSjveA3YRfmmMyKX45Xlzbn4EkeaDkghdrEKIURC22MPvAVNO';
-    protected $key;
     protected $guzzleOptions = [];
     protected $xSecsdkCsrfToken;
+    protected $middlewares = [];
+    protected $handlerStack;
 
     public function getHttpClient()
     {
-        return new Client($this->guzzleOptions);
+        return new Client();
+    }
+
+    public function pushMiddleware(callable $middleware, $name)
+    {
+        $this->middlewares[$name] = $middleware;
+        return $this;
     }
 
     public function setGuzzleOptions(array $options)
@@ -156,6 +165,29 @@ class Douyin
         ]);
     }
 
+    // 视频详情带车信息
+    public function bluevItemInfo($id)
+    {
+        $uri = 'https://e.douyin.com/aweme/v1/bluev/item/info';
+        $contents = $this->getHttpClient()->get($uri, [
+            'query' => ['id' => $id],
+            'headers' => [
+                'authority' => 'e.douyin.com',
+                'sec-ch-ua' => '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
+                'accept' => 'application/json, text/plain, */*',
+                'sec-ch-ua-mobile' => '?0',
+                'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+                'sec-ch-ua-platform' => '"Windows"',
+                'sec-fetch-site' => 'same-origin',
+                'sec-fetch-mode' => 'cors',
+                'sec-fetch-dest' => 'empty',
+                'referer' => 'https://e.douyin.com/site/operation-center/video-manage/self/' . $id,
+                'accept-language' => 'zh-CN,zh;q=0.9',
+            ]
+        ])->getBody()->getContents();
+        return json_decode($contents, true);
+    }
+
     // 创作着平台请求
     protected function creatorRequest($method, $uri, array $options, $raw = false)
     {
@@ -190,30 +222,146 @@ class Douyin
         }
         $options['headers'] = array_merge($headers, $options['headers'] ?? []);
         $options['query'] = array_merge($query, $options['query'] ?? []);
+        return $this->request($method, $uri, $options, $raw);
+    }
+
+    //发送请求
+    protected function request($method, $uri, array $options, $raw = false)
+    {
+        $options = array_merge($this->guzzleOptions, $options, ['handler' => $this->getHandlerStack()]);
         $response = $this->getHttpClient()->request($method, $uri, $options);
         if ($raw) return $response;
         return json_decode($response->getBody()->getContents(), true);
     }
 
-    public function bluevItemInfo($id)
+    protected function getHandlerStack()
     {
-        $uri = 'https://e.douyin.com/aweme/v1/bluev/item/info';
-        $contents = $this->getHttpClient()->get($uri, [
-            'query' => ['id' => $id],
-            'headers' => [
-                'authority' => 'e.douyin.com',
-                'sec-ch-ua' => '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
-                'accept' => 'application/json, text/plain, */*',
-                'sec-ch-ua-mobile' => '?0',
-                'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
-                'sec-ch-ua-platform' => '"Windows"',
-                'sec-fetch-site' => 'same-origin',
-                'sec-fetch-mode' => 'cors',
-                'sec-fetch-dest' => 'empty',
-                'referer' => 'https://e.douyin.com/site/operation-center/video-manage/self/' . $id,
-                'accept-language' => 'zh-CN,zh;q=0.9',
-            ]
-        ])->getBody()->getContents();
-        return json_decode($contents, true);
+        if ($this->handlerStack) {
+            return $this->handlerStack;
+        }
+
+        $this->handlerStack = HandlerStack::create(new  CurlHandler());
+
+        foreach ($this->middlewares as $name => $middleware) {
+            $this->handlerStack->push($middleware, $name);
+        }
+
+        return $this->handlerStack;
+    }
+
+    protected function num2str($num)
+    {
+        $map = [
+            "1" => "34",
+            "2" => "37",
+            "3" => "36",
+            "4" => "31",
+            "5" => "30",
+            "6" => "33",
+            "7" => "32",
+            "9" => "3c",
+            "0" => "35",
+            "8" => "3d",
+        ];
+        $new = [];
+        $len = strlen($num);
+        for ($i = 0; $i < $len; $i++) {
+            $cha = $num[$i];
+            $new[] = $map[$cha];
+        }
+        return implode("", $new);
+    }
+
+    //app请求
+    public function sendCode($mobile)
+    {
+        $uri = 'https://aweme.snssdk.com/passport/mobile/send_code/v1/';
+        $form_params = [
+            'is_vcd' => '1',
+            'auto_read' => '0',
+            'account_sdk_source' => 'app',
+            'mix_mode' => '1',
+            'multi_login' => '1',
+            'type' => '3731',
+            'unbind_exist' => '35',
+            'mobile' => '2e3d3325' . $this->num2str($mobile),
+        ];
+        return $this->appRequest('POST', $uri, compact('form_params'));
+    }
+
+    public function smsLogin($mobile, $code)
+    {
+        $uri = 'https://aweme.snssdk.com/passport/mobile/sms_login/';
+
+        $form_params = [
+            'is_vcd' => '1',
+            'account_sdk_source' => 'app',
+            'mix_mode' => '1',
+            'auth_opposite' => '0',
+            'multi_login' => '1',
+            'mobile' => '2e3d3325' . $this->num2str($mobile),
+            'code' => $this->num2str($code),
+        ];
+
+        return $this->appRequest('POST', $uri, compact('form_params'));
+    }
+
+    public function upsmsQueryVerify($verify_ticket)
+    {
+        $uri = 'https://aweme.snssdk.com/passport/upsms/query_verify/';
+        $query = [
+            'verify_ticket' => $verify_ticket,
+            'upstream_verify_type' => 1
+        ];
+        return $this->appRequest('GET', $uri, compact('query'));
+    }
+
+    public function upsmsLogin($verify_ticket)
+    {
+        $uri = 'https://aweme.snssdk.com/passport/upsms/login/';
+        $query = [
+            'verify_ticket' => $verify_ticket,
+            'multi_login' => 1
+        ];
+        return $this->appRequest('GET', $uri, compact('query'));
+    }
+
+    protected function appRequest($method, $uri, array $options, $raw = false)
+    {
+        $query = [
+            'passport-sdk-version' => '18',
+            'os_api' => '23',
+            'device_type' => 'MI 5s',
+            'ssmix' => 'a',
+            'manifest_version_code' => '160201',
+            'dpi' => '270',
+            'uuid' => '330000000204900',
+            'app_name' => 'aweme',
+            'version_name' => '16.2.0',
+            'ts' => time(),
+            'cpu_support64' => 'false',
+            'app_type' => 'normal',
+            'appTheme' => 'dark',
+            'ac' => 'wifi',
+            'host_abi' => 'armeabi-v7a',
+            'update_version_code' => '16209900',
+            'channel' => 'wandoujia_lesi_1128_0525',
+            '_rticket' => '1637543891920',
+            'device_platform' => 'android',
+            'iid' => '4424888695142616',
+            'version_code' => '160200',
+            'cdid' => '72610f89-9d24-433d-8069-05ea849e7b5e',
+            'is_android_pad' => '0',
+            'openudid' => '694c6a45b8731b45',
+            'device_id' => '71570929069',
+            'resolution' => '810*1440',
+            'os_version' => '6.0.1',
+            'language' => 'zh',
+            'device_brand' => 'Xiaomi',
+            'aid' => '1128',
+            'minor_status' => '0'
+        ];
+        $options['query'] = array_merge($query, $options['query'] ?? []);
+        return $this->request($method, $uri, $options, $raw);
     }
 }
